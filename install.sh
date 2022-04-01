789b465f6bcce6434aa880fbccec56e56f45bee4
Lior A <jongler-dev@users.noreply.github.com>
2022-04-01 16:03:20 +0300

    Save backup of existing dotfile before overwriting it

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ FILES=(.alias .gitconfig .gitignore_global .vimrc .zshrc)
 # Delete old files and replace with the ones from this repo
 for dotfile in $(echo ${FILES[*]});
 do
-    rm ~/$(echo $dotfile)
+    mv ~/$(echo $dotfile) ~/$(echo $dotfile)-old
     ln -s `pwd`/$(echo $dotfile) ~/$(echo $dotfile)
 done
